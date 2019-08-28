57fcb0538c7e7f986498847eca79e3ddbf068e28
Cessalan <31516973+Cessalan@users.noreply.github.com>
2019-08-28 19:51:55 -0400

    Add project files.

---
+++ Sheet1/Controllers/coursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet1.Controllers
+{
+    public class coursesController : Controller
+    {
+        // GET: courses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sheet1/Controllers/testamonialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet1.Controllers
+{
+    public class testamonialController : Controller
+    {
+        // GET: testamonial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
